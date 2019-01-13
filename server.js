87dbc9f7f105af89ff6b0c330ceeb3b2444ffe3b
vikrant <vikrant9031>
2019-01-13 17:56:22 +0530

    v

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var fileUpload =require('express-fileupload');
 const postgres = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl:true,
   }
 });
